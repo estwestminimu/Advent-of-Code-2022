3d493c65374b7507e9c70785b3fe5a7e72085836
Szymon <96234081+estwestminimu@users.noreply.github.com>
2022-12-03 11:07:55 +0100

    Day 3

---
+++ 3/js.js
@@ -0,0 +1,530 @@
+var array = [
+  "mjpsHcssDzLTzMsz",
+  "tFhbtClRVtbhRCGBFntNTrLhqrwqWMDMTWTqMq",
+  "LltbngLGRSBgSgGRCJdSdQHvdfmQccmjSQ",
+  "lBslsZDDWdGdGpSMts",
+  "grQhDvqLQHDNGJJtbRMQQJ",
+  "HChCTnnLCgCrTZPPFzzVPcVD",
+  "ShrzjhNGrNqrhWnHHfVHbhnHbbhH",
+  "RBsvcBcDCdsRTsvgCgcPFRQpVQGQJPVFbnJfbJ",
+  "DvsTsdlCBsGLrjzmlqqz",
+  "WJJqZTgCnBLGCZBJCJnTLggTDDSDDMNdDSdbdSSsWDFfMsFf",
+  "PVjqpVHmPpvmcjhrRprFmQQffbfNbQMMsSMQNQ",
+  "cwcpRvrVlVgwtBwZqBzZ",
+  "qfJJmpqpmhsggvvpVPZCrhdFLFzZFDdLLh",
+  "CtCTBctGcGLSzZddGZSW",
+  "RlNjBCnjttBHHMMcQHCsRfsbfwgggmmJvmgfpm",
+  "ZmcgBBZhZMsnqnCPjpHPjLHp",
+  "dGbNwNtlTMTzGfNvTvdwNGVLPpQHPjLQPCpCjPqjLbpLPR",
+  "dvDTdfvNBhDZMBDZ",
+  "cvvRvbqcllbBVlvVVbVVlbVDjRjDjdMsHPZPGdDPGPHrDP",
+  "FwtpfwJtWwNtTTNnwFCtjDJsQdQPPPPMrjrPJHjH",
+  "CwFpnppgntShgbsscbms",
+  "cWMFMQpFNcvNDdBDgdsT",
+  "MPrrfrCHBBsDZCBJ",
+  "LmLjMLjjLWpVcRVR",
+  "ZrRZqlZMqTWrMDqwvnvVtnsvddvVnlVf",
+  "pQNhhLNNGmLjhhcfvndDpffdfdVf",
+  "QGjCLCQGmNgPBQDFFgTMJWWwMRTrTZWWBWTr",
+  "WrZWZPHHWZHprZVmVvqddBttBBhGhtvh",
+  "gzDlMTJDMfqhBGllhl",
+  "jJLqMMDDbbqjLpPHcsHLWZspPr",
+  "bsSVRVGsrDstrrSjcQjcjlPwzjQl",
+  "gHBggFNTTvTgfqgCFzljWwLWQQQnrwQWnf",
+  "NvJHgpgHvqBhNBJhHTvpBCJCZmtdpDsGsZdZMZRbVbbMdrZs",
+  "MPPtPwPnRnMPPnwrtNSGgLSCGGGNSLtSgD",
+  "hBhWFjfCsTbbbWqFFWBBqBhsWZVGSVglZHLSVDlNWDNHHGgV",
+  "zsCfTsTCMdmRPwzQ",
+  "JVQVvvszzvTsVsVJjctppcCtjtPRcTlP",
+  "MdFgqSddMqMDbtDlNjRDSR",
+  "qFZWZqwHlZfZvzvZfLZn",
+  "vpqwQSsHSHDQzDpgzwZlRLRZRRZTnTrrvGhh",
+  "JBcdmbmFMPgPbgfrZRZnRFFnrnLRln",
+  "JNdBNgbdJmPMWSSDzwVDtwSWWW",
+  "BDMcDDppHCStpWcHBDNtzPJjqGlllPMJzPGjwjlq",
+  "CZdZLmgCdqbPzjblZj",
+  "vndLfnghRQmVrhdvgBHpSCDWHBBCVHNppD",
+  "WrhrJJGSWzpTWwts",
+  "VlLPmqgmRNZRGwsvttjgcwsT",
+  "PDZmlbdVqLmPlddVNRDmmmbbSFHrCFQCnFBFJHSJGrDQCBrr",
+  "hvPdpvhHvHvPrNfVhDfjggFfRV",
+  "zlGwJGslsSDRfjsg",
+  "MJMWjMJzwqWGzJwMqJBTCmHndPPdCBvmdCpmHn",
+  "PVWFpQhJhFJpGbRCvRHGCp",
+  "jgslDjftsqhNglTgllgTqMnlHwCcvwZwRccSRCbGSGbCMHRw",
+  "TgjhNNnjlTfjTdDqTfhjnmzmWPzWrLdrQBPJFWJWBB",
+  "qPPRMPlfSzSSSPPnnLnqMlpQQtrrtmWpbFtQrdzrtrWt",
+  "BBvCcwsVThsBgswDBCFQHQpdmQvtrFpWFvWp",
+  "gCghTJgVCgDGVMlRGMqZnSWqlM",
+  "RWbHvrbHBsbWBHJWvJwMtmdZwdtmdvwMZQff",
+  "DRVjcqhRchhGGllhCgdGQQzfttzGQGwQfg",
+  "cDRljchpqTcjDFTFVcPcPCWBHpNnJNNSnbWbHHrSpWHr",
+  "dtHrRrBHrCRhddftjgBrRhgjsbbbMpbSWSTjWcsDTWDbcW",
+  "GQPFVQVQnJlqVMDcMzpDfzpDVD",
+  "qZZJFLlLnvFFGPGLPqnJvwQldfgHrBRBmBhgNBRHghNhhwRg",
+  "rLbrZhPgqZhMdVFSFTSGCqFG",
+  "zsszfRzjtHtzvRTSDdFFCtdDdtND",
+  "fcwllfmwzRHlfmmzFvQQLrgLMLBZhJQZPrZhJLhW",
+  "sllrCfpQQJpMHLgzwDwpNqzzVDpV",
+  "RZPFZPGcSMFtGPRGMwNDVwdRgzvNwgqNvg",
+  "hBmbMcBmcThmcGtSFTZfQCJjrHLJfsjhWJssJl",
+  "DqGCbGfCRhfZCVbbqDJJGJBgRNpNdpBNNgNBBNwHnRgt",
+  "rcWSsSSPSQtwBwHD",
+  "MLscLMzvvTvcTLzvWWFDPTTrGqmFGGqCZJGbblbVbVZZVmFJ",
+  "FprpsLQTrstQHNmVSVml",
+  "JMggWPggWcRbwgJPCGMcGcfmzHlMNSjfzVNhHfVtzSMz",
+  "cwnPnBwgnGRgRCgRbWJLpFsLtFBLFrDLFZZDrL",
+  "lVgjLLLMgFMDCwCFqCRbngsvnGSvnSGndbsfgf",
+  "WZJcTWcNTmJZphmTJJNQHcdvfdbvnRRGbGthdrbttfSv",
+  "ZPQTJTpTNPJNQTmJRBZJNBHjwMVwPCMwVlVzjwwzqqjVjL",
+  "hznNhNQNQFDWVFmDQm",
+  "SMqZBMMbBvDbHPzzdVPH",
+  "zzzTBTMLNTgpnTTh",
+  "NLCdmsdCVLGHCHdQzzmznnFwRjFMDMwpTBjDRpnpTBMJ",
+  "PrcfcrglcfWbSqgrlqvShrwpJpDBFJHpBWjTDTRTRTTB",
+  "crSgSHtPttfdLGmtzzZNNV",
+  "BTlTVqCBqtTcBqVhWlsJjDvsnLsvlvpJPj",
+  "gMgggGZbSMzNRRRLmZZnQZQPPDvnsnDvJwwQ",
+  "dMRRmMgbNfRgmfSdGFgNgTBtrhrhqfWtLCCWLTWWHc",
+  "zcfVrPwnwrPmrvnjdFdBbHFFdd",
+  "CCqpSSQQpQZLDCSHPpBFvFBjTHRvRR",
+  "DMLGthLZMLtQGhGNMPqGSDflzfwcVmzJzsfgNVrswcrr",
+  "hSgvMTQvChSqPvhTrRLlVHJgfgRJlHHHJH",
+  "jmzsZzZzwmmLGGtwtVJWNNDRDtVcfVRl",
+  "GnBBLbzzzFszBFpzvSdrQQCTCQbhMvSQ",
+  "VHpTMrZMMbDbbpTZmQmTnmzhTqjqlWWQ",
+  "GGvgNsvNCNvvGvlqqdzWZmlsmZqZ",
+  "wNNNgccNGJSNBSRNBNvNcvJHLDDZMFRMppMLrfHDLbrrHF",
+  "spssbPMLpPllspGNsNWMrnwddnfcqrnwwwwMwM",
+  "VmQBFCjzzjmfnwbrngcVrd",
+  "FQbSFjBvvzsWvWGlvWNl",
+  "JLFSwfwRLLfGhnQJBQshvn",
+  "pZgNcpCWpWtcvhjGGjtVvszD",
+  "CccMcPcgTTCWmcZcWMcmTNZPmHdrqSHFRRrqwrSrRqwrHmsH",
+  "BPMhflJRhqnPNGjNRNRjgSRm",
+  "VdVsDswTVZbCwCZBrcDCczTwtjtNNjmjmgpmjpQggpGVSgQm",
+  "sTbWrsTBbrTPPnqlJnPPhW",
+  "nvrgjMWBvQWPvQnsZfGcZcRFdGFtdtZB",
+  "bHVDwmqNNDhHNzqpphLNHVLpSJcdZtfffRZdDgRFGSddcRZt",
+  "HNLNqNqLNbhqVVbClngjnQWPTWgsCgvT",
+  "tfstpcScscBTFTpFnsWSmgdzJlgmgBmPPzJmvdPm",
+  "jnrqrLHRwGrwhdPvvPvhjJmP",
+  "qqCLRCGrZZqCHRVtVWQptFWppnbcWb",
+  "wCDJZJgDwHpdqHhdGHBhhH",
+  "WSPmJMlmbSmztQlQsvPhnhGGdBddBqdGddTbVB",
+  "WzWQftWMSWtmvmmSWtMQPgggpZwLwZjggJFgrpFCfj",
+  "MvQBJMBQhjQFNFnjnj",
+  "dtlZmRtLmjSTSLLtTtNVwWzDRzDVwwWFwnNn",
+  "dmmLCqTdcLqtLGqjBhpfHqBGpv",
+  "PBPRhjTPPlLRBvlvfwffqJGfpG",
+  "rHtMtrszFtSgbFrrggrFgMnwWGzmQqWvGWzGQpJGfNqqNz",
+  "FggcbSMntVgMdRCwZcjChLCT",
+  "lCqqBlCwlnDqPZTZZBLNdjJLwttNWjjdzJzc",
+  "fVfMbvbvmbVsmSsmMVWNtzzcjgLWgjztMMtg",
+  "VVmFhFRSfbQsvVQmvSfhSsmzHlCZqrrBrDBrHZPRTZnnzB",
+  "CRrDWmzRRQMmDqrrBgBQmtHljhHwtwlwplcBjHGwwB",
+  "PWfPSWnvsNZSZdfjHjZtGHjchllltl",
+  "WVsnbSPTbNdbmqTQmmrmLzTq",
+  "cGtMBGSJDgtgMBsBMgMvWWSHWjpjzHTWTPpqWzqW",
+  "mNVQNsdVsdhLmCpTWWjmCjTT",
+  "NQQwrfbQrNQNbrrdLwfQsZdgFbBBFBgggRGRDMRFFMRDgM",
+  "lFnqgqWQvHWqgvlVglvqjPjcLdfLfBPLnrbLNLcN",
+  "hmTmthppsRtpTRRTZMpSbLdNjNcJLcrcBNbJBZZc",
+  "smmpRsTtpSSsRGhppmmhdCMGWwqFQgWGWWDgWwVFHQqHgg",
+  "mWFjmcdcFWcSSQjzrpvrwRGvTwQGGG",
+  "HRJfgMZVhtRlHJHBVJTGvGppbpbvvGTvTtrv",
+  "glsgVMVqffdnPRDcqLnL",
+  "MtvLJdmLLTvSSCtSzLSTcDhRjRftQjjssshfQNjPtf",
+  "nlggrFWzRsfFjVQN",
+  "WgwwBgbgZBHGBnccTzMCLTZJmLLL",
+  "sRtHTBBHZtDTtZhdPzWdGcdVFdJmGcnm",
+  "wpwMLWCgvfNvwvwbbCrwgfzPncrJPSFVGPnrcJSVznmV",
+  "bLpvwQwMwpjWMgfvgZTsDsBttqHRjTqHlH",
+  "mpmGpCpmlpmwfmCQVppCVfQSSjvSqgWvvvDgNwWDgnnDnW",
+  "RBLsHRJBRrHJWFDWSNqFWj",
+  "zZBLdsdcZrsBjGfpGVpTTPGlVc",
+  "NBbTzgwSNmrFWpVrzrFM",
+  "LnZQtQlZVnMrFBBG",
+  "CCdtddBtPdNqcvHSCCcg",
+  "ZFbZPHbZPTQVVlsGNF",
+  "qtvDWvgRftqGNccCNVThDs",
+  "fRwGBBjBppdMdBMZ",
+  "GffflsZsPZVfjsssNfZsJNNZVcMDSqMWFcwFMMpcTMTTFSTS",
+  "LhrCmvzcRbbhtmRdTCMDwWMpDWqqqMpW",
+  "dvRQmBBvLzBRRvRhhcdbhdRgjHQNllJsfsNlZZljZGGNQN",
+  "wjbMPsbfLzVCTMVbjLplmpshhSpHShhJhtsm",
+  "ZrcqZTDTGDqFdJtGmdGSpl",
+  "QNNrWvQRqRNWnTQRvqjPbjfWbCBCMbMLBwMV",
+  "wRPRsppFfWJRlPRPFlpJfwSMzzZTBwBtZTTCMCMtdz",
+  "vGLGrjcfrLVGjfnGTMCMtNNnCTnMtCBd",
+  "VrjqhjhLVcrGVRqJmqQspmfFWm",
+  "LRfdnmwMwdSBmfvJNrrgLhCNgqqJWs",
+  "llctPPVTcPStgJgshCsrCs",
+  "DpTlFpFVRFZRFFSv",
+  "sPgRgsmdcqmgSvvFRRRRdqdFfTWZhhdZrZbbWfTpwDfbWTbw",
+  "jLCCHtLljJzjlplfZSlwTfprZZ",
+  "tBHVjQHzHQJBtSVmvRsvvFRqnGgv",
+  "spppVDbVcbgVSFgFZZbGZgbJMRBTvHTvJJHGtHRwtMGvHT",
+  "LldflzQLLQmQWQQfnwMWwHJtTtwRBcBt",
+  "CPjfhCmNmNfFVchpchFVhp",
+  "bZQJgQmQmTgnLBRtNPNnml",
+  "ccszcqldGzhszrVsqdlHVNwLpppwHPHRtBBppDNRLt",
+  "VSzVhVdcfrrhcqGrVhrssQQlMbJvFjMgbFSQggCvCv",
+  "hHWVWhhlZDZVWNTgczWLjbtcTFFj",
+  "JJnPnCdBCBnnRCjSsjStBgsbFttb",
+  "MRpgCpGqdPRppJwpnRqRfZZhmvhHDrhllDHhhZGZ",
+  "SPcgLDcLLnWFWCNVCRPT",
+  "fhZQtsbtmbmfZTVTVRWfNvTCTT",
+  "jhbbmzRsQzpLDcgLHLjg",
+  "GSFRHrCCGRJDJtrgWdrL",
+  "stcVQshQZBsBmjMsZhmMQQWDDvNWdncNWvzLgdDnzdDN",
+  "sVwMBQBhVVjtQZVPlSfPCfwRpSCpRl",
+  "bBHHJMJvBvWMJWqqccNNPhMCrclChQCC",
+  "RPppPgfpwgmcQgrhmm",
+  "tfwTwpFPGGwZSRtpVjJHbHLvSvLSqVLL",
+  "jlJfZGjljJPBqJGnfGVMqGfrFWWddvDmFRDcmdFDdDvbDM",
+  "hTCTsgsgwhTbvRdcFmsddpFd",
+  "wbQNHTQLgCwSThhCgwnZnJfqnqJBlNlBnnVl",
+  "CLlfbjjbLlbbDGbLzfCGhdtdWBthdBWsHvWHBnntWs",
+  "rmJRJFqrDwVFTwFmSJvtvMtdJMMHBdBBndWt",
+  "ZVrVVZpgTpZFSqmZqRNlNNfQQbpGjDQbbpPl",
+  "mVCrhGHGmZhrNlDwbWnLWWvGLWWwnd",
+  "PNsqgzspsgNFJNFfzqpWSWdwSvSPnvdWbSbvjd",
+  "NzgJzqMcgscQqJcpJRzBmlrBRBDDlHZBBBHtHZ",
+  "NJmNJDwcMmJNMbJJDNDqcGcsWRWHQzRPQjZLRGZWLQsjZQ",
+  "dgSnTBgdpddtgShSTZjLRhRLHqWPPhPRPQ",
+  "VgdTpBntlvBVrlfcbqJcMrfmcqmb",
+  "wvqwvPwNJgFmLdvDJFDmDLvJlQZpMzSpBVflpdSSMlQnfldS",
+  "WjCcRZCWRjjRtsZhRRhpSVBVnzplBfWnfBfSQz",
+  "CbRbcsjHZrhbTRtsGbCrgNgDDPFFqvvJvJFDFw",
+  "GlsCrbCChShqgqlbSCcVbqgVhBwjBDFBhBhdDWvwBFFvWvDv",
+  "THmHMmtMnLfHRnzRZnfLBDWWsWzWFNsvWjjvjvFF",
+  "mpHRtmZffHTTMpmLMLLnJtJCgScScsPlblcpCrPbblCPlq",
+  "vscDLrcvrsLNStdTfBCvgJTqGBdd",
+  "bwLbzRhbbdTfbgCB",
+  "pplQzLwmPZVMStcDjFtQrS",
+  "RMjCrhFJhRVRVCCFFsvmnvqrmbvqmqSmbrvm",
+  "tzfpBgTHzttGzZpBfHGDBZHbccnGqbmvdNlGnSnlcvSwbn",
+  "pDWTHDTzgTfWZpVVsWSPjRFSMsFs",
+  "fmrfmrwVfjmrzjqCsqqvjsvvpG",
+  "hFDVtFStVtJnPPtJNHbtQWGbQsCvCsQgpWGggdQC",
+  "NBSDSNHStHNHnhStHNNrcflrmTzBlwmzrlMVVw",
+  "SjtZZSdNcDldPQqndl",
+  "BbgzgWgTmTBfwrbnDjQDwVPwDlnsVq",
+  "zBBrCTTMBWLMWmfMfbbmrMtjNZLFJRRZSSvFFtStvGGJ",
+  "CTCGLGCFRRSMGnZnLCTfdffhpbNbDfpdZBvhdv",
+  "rJlqclVPHJWVrgPPQqjqgJlhBhDBBQdvbhwvNfhswfNpvb",
+  "tltrcrHjlVWVCDzSGCCzLCGt",
+  "sbHHsbCCHbLSVfJbbfSLNJBzvzMMPrhPPNztZlZNZhdt",
+  "GTWjplTgDnGmQGpQnQhZrvvBMPztPzvrzvjZ",
+  "mQgGWllcFcTFmgwcDppDQGTCqfsSLqsfSbqJqLSSFsbRfF",
+  "jslsFjLLLLvFwWtQFTFDJQWp",
+  "dGzdrNmRWqVBGcTbwpRDRnbJDRhT",
+  "qzqzrrPNNrmfLPsjglHjgW",
+  "QjCHcPfcgQSgPPcffQSmmmLmrJJpNpBMrJMtFrBBBMFrrpNS",
+  "VGVZfDbbVVZWGvDbFrlBZNJBNlNMwwtM",
+  "sbvfhqTGTRnhTVGvzgHmgQLQmPqzmPLm",
+  "sLwnMHnbnLMjGpZsjGGtpc",
+  "ggvJrNNTQgQrNvgqBqZCCjClWjGtWjCpGJFW",
+  "TVdrqvVrTNTzBqQQzTRMfHbMwMbZMdMbHwRD",
+  "bcfJQQJHsQPCpdpWdPbb",
+  "RHjHDwZtrZmRDDtwtjRBVFdWVrrrBClldVphCF",
+  "zDgwgNzjmDnMnzMMHncG",
+  "vMHRvMhvHWRBRDHhRBwWvRBqLqbGwqnqnnNTbNqdNbbVVr",
+  "pslgcZszJltrsZcZgNnnqbSSTSSndbNbzS",
+  "cZcgsZgZZgPgmcpfJtfttWBQvmFWjDQDhBmFjDHvFr",
+  "bVbBvdTTVLbCgCznLJsJcwHPczfz",
+  "NFcDphSDrFjGtZNZjplZGZFnzPHPrzHHzJMnnwfPsPsRJs",
+  "cGtGljFmWdvqmVCV",
+  "qSNbTvcvTGTvGcgtBNvcbdrdjrnjRnjRVHdDqHrRHj",
+  "ZZZZPLWPzPDCCsCRnRdwVFnjdwPVFP",
+  "ChlCLLZftfcBvfDv",
+  "cRtfctVgmRclmBFGbbMBDDFPtD",
+  "svQZhHSHssjTvjpQjSSBBMJMJGDBpPbMzzpGzP",
+  "ZsvsCTWhCHhSwwjrwbndldlRnfRNmb",
+  "PQdTgdGpRcTccCfj",
+  "hHFLHlHBhBlmlDFzHrhhfZNZbfNZcVWNVVZRDjCC",
+  "LFLLMHJHSBhBFGGnMMvsGtGGtj",
+  "fwmVnVCDVqpNQqqb",
+  "ddBcZZWdvGWzBzsWvLvddlNHcHQPbQqqJQNNQHPHQT",
+  "WgGvsMMzvgbntDhCmt",
+  "JjwhFMmwjJwmCgTgSCSFlPLg",
+  "WWbsbVtftBZWtnWtncbQvctTGLpLgCpzPPPlllpzlgPPTQ",
+  "TBvnfBffWsfVtTvbZBTNjwjqddhMNqwRMMhdRMrq",
+  "SllrbtTSQrSQrbrvvMvzFDsBsssWpWdWbGpGBWNWNW",
+  "hhCfmmmjmPLCfmnPLfqPgqqNNBpjZBZQDNQdpWNpdBBsBp",
+  "RhLfPhQLQfCRnHfqTHHrFJMrttTwTtzt",
+  "BFrFBJMMJnnsNJBFCdLCnmvzbPdCmPnc",
+  "LDLVHQRfDvdHdcCmcv",
+  "llQDwqSVLwZLZSgsGZMNMgTjTMTr",
+  "mrwdbqRhdCNGgZBHbH",
+  "jVTPMjvjpvMfTfQfPlpHHZNnNBHgZDGsGMnCsZ",
+  "TLlfQpffQvvzhtNqztRFtzcm",
+  "DDfvJZZPDHVPSPcSvcgcWCsWQcTTdhQTTh",
+  "dMwpbdjRtrFhhTsTFQWqhC",
+  "bGRdNpbzlvLfDfZlLZ",
+  "bdPQdcpdbpjFqpQcQwqqhhNRhJvWRfrrWBsJrfwN",
+  "mMtlZfmtnLZtSnGDlmGWRRhrWLhJsBRvgRWghh",
+  "DnMGCtmCzfGMbjdQbVzpqcFH",
+  "jwnGggRBvvpBZCljCsCWrhhrsh",
+  "FVMcFLqLMqcJfVtDqMJcHMHWCSblzzrWsdhSLlSzbrGCLz",
+  "HQVFPDtDQDFFNTZpPgNnGgNn",
+  "HNBHNqlqHJQBRNvdmZvmPdZZlpnT",
+  "bDbbhDgSfzVVfnvPmfHmTZZd",
+  "jgzbwrhVsDgsDWLwJqqBMqcqHL",
+  "tzNtJzsJVBHzbjbglCHc",
+  "nfmnGnmPhntCgHvtvmCj",
+  "MStTwrMTWrTdBZSNLZJNVQ",
+  "NVjmwmVGGwGFHstwFHMhTh",
+  "psRSzzscZscZpgQQzqQtBBHTTlThHHtTTh",
+  "rCprbpZccggcrRzbbRRbscvVVWWvNfvVWnGDCWVNddmd",
+  "rphfGDgtPtllrPlFlGrhGjnmnTnjcBsncBBVpTTBmc",
+  "SqqZMJCLwgCwJgQRqqgZQNwdBBsBBHVBdTHNsnVNBTccnc",
+  "MJqZZMbqgzRCSJZwPtFfGzWhrrfGttWl",
+  "cSZqqcwbqVzqCbqVqVZPsvvDCDrffngvphggndhdGh",
+  "tTNTMWJNQJHMNGSSprfdGnfdth",
+  "WNRHWWMJSRWzswbczsVPRs",
+  "HCgcSMhSMBGMdvGf",
+  "RNQqbDQqFdRFdmTZfGtPZvtGlQffll",
+  "mNpdNrRDbTNrmbpzmpWmbpWcswhcHcjhscSHjSgVHwHn",
+  "MwgcFgwMMcscCbMFsMFCgMgPPLWPvptvBvPvtvvWmBBzwG",
+  "nhQQjTJRVDdQJrPpmnGGBmvtGvLz",
+  "HdJQdJHjrJQDBQjhQVQJhdJcqlFHcSqsNbCbCqCHFqCFgC",
+  "JvTnvWtdJLbhJHbMwwHjcGHCwHwQGQ",
+  "mqtmsllmfqVFwMwMrrPjmQrC",
+  "lfztRZSlRDRVzfdpWnSvWhNdbnpp",
+  "rSvrgggzHTNzrHtnptpmlDngZjWj",
+  "MdMhqMhsfMSRcGqRsQQRctjjdDnjtjClCjjpZnDlnt",
+  "BBMRsQRfRcscGqBfRRsBssPBLLzNLFPwvVFFPTLbbLwHHTvS",
+  "pCmCfdPFzmsFsDhFFDsttptpRtJjLnlJRtttHt",
+  "ZQwgWZgqJhTTRtgV",
+  "GNqWNvcqqQQrMMWcQzDDsSzBDBSssSmhhr",
+];
+
+var array2 = [];
+var out = [];
+
+var i = 0,
+  z = 0,
+  p = 0;
+
+//dlugosc tablicy
+for (i = 0; i < array.length; i++) {
+  //dlugosc stringu
+  for (z = 0; z < array[i].length / 2; z++) {
+    if (z == 0) {
+      array2[p] = "";
+    }
+
+    array2[p] += array[i].charAt(z);
+  }
+  p++;
+  for (z = array[i].length / 2; z < array[i].length; z++) {
+    if (z == array[i].length / 2) {
+      array2[p] = "";
+    }
+
+    array2[p] += array[i].charAt(z);
+  }
+  p++;
+}
+
+var u = 0;
+var v = 0;
+var z = 0;
+var f = 0;
+var flag = false;
+//w array2
+for (u = 0; u < array2.length; u += 2) {
+  //w stringu
+  for (v = 0; v < array2[u].length; v++) {
+    for (let z = 0; z < array2[u + 1].length; z++) {
+      if (array2[u].charAt(v) == array2[u + 1].charAt(z)) {
+        out[f] = array2[u].charAt(v);
+        f++;
+        flag = true;
+        break;
+      }
+
+      if (flag) {
+        break;
+      }
+    }
+    if (flag) {
+      flag = false;
+      break;
+    }
+  }
+}
+
+sum = 0;
+
+var p = 0;
+
+for (p = 0; p < out.length; p++) {
+  switch (out[p]) {
+    case "a":
+      sum += 1;
+      break;
+    case "b":
+      sum += 2;
+      break;
+    case "c":
+      sum += 3;
+      break;
+    case "d":
+      sum += 4;
+      break;
+    case "e":
+      sum += 5;
+      break;
+    case "f":
+      sum += 6;
+      break;
+    case "g":
+      sum += 7;
+      break;
+    case "h":
+      sum += 8;
+      break;
+    case "i":
+      sum += 9;
+      break;
+    case "j":
+      sum += 10;
+      break;
+    case "k":
+      sum += 11;
+      break;
+    case "l":
+      sum += 12;
+      break;
+    case "m":
+      sum += 13;
+      break;
+    case "n":
+      sum += 14;
+      break;
+    case "o":
+      sum += 15;
+      break;
+    case "p":
+      sum += 16;
+      break;
+    case "q":
+      sum += 17;
+      break;
+    case "r":
+      sum += 18;
+      break;
+    case "s":
+      sum += 19;
+      break;
+    case "t":
+      sum += 20;
+      break;
+    case "u":
+      sum += 21;
+      break;
+    case "v":
+      sum += 22;
+      break;
+    case "w":
+      sum += 23;
+      break;
+    case "x":
+      sum += 24;
+      break;
+    case "y":
+      sum += 25;
+      break;
+    case "z":
+      sum += 26;
+      break;
+    case "A":
+      sum += 27;
+      break;
+    case "B":
+      sum += 28;
+      break;
+    case "C":
+      sum += 29;
+      break;
+    case "D":
+      sum += 30;
+      break;
+    case "E":
+      sum += 31;
+      break;
+    case "F":
+      sum += 32;
+      break;
+    case "G":
+      sum += 33;
+      break;
+    case "H":
+      sum += 34;
+      break;
+    case "I":
+      sum += 35;
+      break;
+    case "J":
+      sum += 36;
+      break;
+    case "K":
+      sum += 37;
+      break;
+    case "L":
+      sum += 38;
+      break;
+    case "M":
+      sum += 39;
+      break;
+    case "N":
+      sum += 40;
+      break;
+    case "O":
+      sum += 41;
+      break;
+    case "P":
+      sum += 42;
+      break;
+    case "Q":
+      sum += 43;
+      break;
+    case "R":
+      sum += 44;
+      break;
+    case "S":
+      sum += 45;
+      break;
+    case "T":
+      sum += 46;
+      break;
+    case "U":
+      sum += 47;
+      break;
+    case "V":
+      sum += 48;
+      break;
+    case "W":
+      sum += 49;
+      break;
+    case "X":
+      sum += 50;
+      break;
+    case "Y":
+      sum += 51;
+      break;
+    case "Z":
+      sum += 52;
+      break;
+
+    default:
+      break;
+  }
+}
+
+console.log(array2);
+console.log(out);
+console.log(sum);
